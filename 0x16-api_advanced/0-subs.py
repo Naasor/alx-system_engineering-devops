d1cde7cb4588f410ef99ac1a3d25e662f5427d9b
amofasandra7@gmail.com <amofasandra7@gmail.com>
2024-04-09 23:29:17 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit
+"""
+queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit
 """
 import requests
+
 
 def number_of_subscribers(subreddit):
     headers = {
