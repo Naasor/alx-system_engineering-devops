fdfbaff891613579c47ef7389b2b945a56f2d8d7
amofasandra7@gmail.com <amofasandra7@gmail.com>
2024-04-18 14:31:22 +0000

    the sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,13 @@
-# Fix the nginx requests limit
-exec { 'upgrade':
-  path    => '/bin/',
-  command => 'sed -i "s/15/4096" /etc/default/nginx',
-}
+# Increases the amount of traffic an Nginx server can handle.
 
-exec { 'restart':
-  path    => '/usr/bin/',
-  command => 'service nginx restart',
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
